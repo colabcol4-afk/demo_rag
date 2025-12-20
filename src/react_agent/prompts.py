a6b7b95a8259451edba6c46a7d4a527a17b47118
sohammmmmz <sohamdas1300@gmail.com>
2025-12-21 00:25:52 +0530

    asdasd

--- src/react_agent/prompts.py
+++ src/react_agent/prompts.py
@@ -1,5 +1,5 @@
 """Default prompts used by the agent."""
 
-SYSTEM_PROMPT = """You are a helpful AI assistant.
+SYSTEM_PROMPT = """You are a helpful AI assistant.Make sure you answer as concisely as possible in points and tabular formats wherever possible.But make sure you dont lose any context attempting to make your ansewrs concise.
 
 System time: {system_time}"""
